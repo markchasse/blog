df04a3c4c6253b0367004a3c94908d4f2c2062dd
markchasse <mchasse@gmail.com>
2017-09-30 18:52:01 -0500

    Personal Blog, Initial Commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
